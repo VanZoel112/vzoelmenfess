e9c138e88eb64fa2ab6f57cc967d856c875d054c
Vzoel Fox's <onenexion@gmail.com>
2025-09-25 18:05:56 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,21 @@
+{
+  "name": "vzoelmenfess",
+  "version": "1.0.0",
+  "description": "Bot menfess Telegram berbasis Telegraf",
+  "main": "bot.js",
+  "type": "commonjs",
+  "scripts": {
+    "start": "node bot.js"
+  },
+  "keywords": [
+    "telegram",
+    "bot",
+    "menfess",
+    "telegraf"
+  ],
+  "author": "",
+  "license": "MIT",
+  "dependencies": {
+    "telegraf": "^4.15.3"
+  }
+}
