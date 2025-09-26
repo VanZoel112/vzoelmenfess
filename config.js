934bdb68607377007152f932167468a91720e194
Vzoel Fox's <onenexion@gmail.com>
2025-09-26 10:23:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const config = {
   botToken: '8305166785:AAEAQH2exa0CyJKWso78L8tDpDJOQkG9Iho',
-  channelId: '@VZLfxs',
+  channelId: '@VZLfx',
   photoBoyId: '6244774387463081116',
   photoGirlId: '6244774387463081115',
   sendDelayMs: 2000,
   forceSubChannels: [
     {
       id: -1001999857761,
-      link: 'https://t.me/VZLfxs',
+      link: 'https://t.me/VZLfx',
       label: 'VZL Channel',
       buttonText: 'Join VZL'
     },
